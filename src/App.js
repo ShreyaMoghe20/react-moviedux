66b7d0adc9cbf611d696c51c5ce011727a65b238
ShreyaMoghe20 <shreyamoghe.2002@gmail.com>
2026-02-06 14:11:23 +0530

    Add homepage and gh-pages deployment setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Footer from "./components/footer/footer";
 import Header from "./components/header/header.";
 import MoviesGrid from "./components/movies-grid/movies-grid";
